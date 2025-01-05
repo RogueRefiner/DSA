a2118f2157298e02f68c9d4b2c458dfaa72cec8c
RogueRefiner <arenasignup@gmail.com>
2025-01-05 15:40:35 +0100

    fix: repo structure

---
+++ SortingAlgorithms/counting_sort.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+
+
+def counting_sort_default_dict(array_to_sort: list[any]) -> list[any]:
+    d = defaultdict(int)
+    sorted_array = []
+    for num in array_to_sort:
+        d[num] += 1
+
+    for key in sorted(d):
+        sorted_array.extend([key]*d[key])
+
+    return sorted_array
+
+
+def counting_sort(array_to_sort: list[int]) -> list[int]:
+    count_array = [0 for _ in range(max(array_to_sort))]
+    sorted_array = []
+    for num in array_to_sort:
+        count_array[num-1] += 1
+
+    for idx, count in enumerate(count_array):
+        sorted_array.extend([idx+1]*count)
+    return sorted_array
+
+
+if __name__ == "__main__":
+    print(counting_sort_default_dict([2, 3, 10, 2, 7, 4, 7, 7]))
+    print(counting_sort([2, 3, 10, 2, 7, 4, 7, 7]))
+
+    # print(counting_sort_default_dict([False, True, False, True]))
+    # print(counting_sort_default_dict(["a", "b", "a", "c", "d"]))
+    # print(counting_sort_default_dict([2.0, 3.0, 10.0, 2.0, 7.0, 4.0, 7.0, 7.0]))
---
+++ SortingAlgorithms/merge_sort.py
@@ -0,0 +1,28 @@
+def merge_sort(array_to_sort: list[int | None]) -> list[int]:
+    if len(array_to_sort) <= 1:
+        return array_to_sort
+
+    middle = len(array_to_sort) // 2
+    left_array = array_to_sort[:middle]
+    right_array = array_to_sort[middle:]
+
+    left_array_sorted = merge_sort(left_array)
+    right_array_sorted = merge_sort(right_array)
+
+    return merge(left_array_sorted, right_array_sorted)
+
+
+def merge(left_sorted: list[int | None], right_sorted: list[int | None]) -> list[int]:
+    if len(left_sorted) == 0:
+        return right_sorted
+    elif len(right_sorted) == 0:
+        return left_sorted
+    elif left_sorted[0] < right_sorted[0]:
+        return [left_sorted[0]] + merge(left_sorted[1:], right_sorted)
+    else:
+        return [right_sorted[0]] + merge(left_sorted, right_sorted[1:])
+
+
+if __name__ == "__main__":
+    print(merge_sort([23, 42, 1337, 98, 1]))
+    print(merge_sort([23, 42, 1337, 98, 1, 75]))
